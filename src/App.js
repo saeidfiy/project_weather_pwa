f3840eedb97aa674de41df18d5d2ebc52d487be0
Saeid Nasiri <name62777@gmail.com>
2021-08-14 15:29:33 +0430

    handle loading on project

--- src/App.js
+++ src/App.js
@@ -1,49 +1,49 @@
-import React, { useEffect, useState } from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
+import { Loading, WeatherView } from './component';
 
 import { fetchWeather } from './api/fetchWeather';
 import './App.css';
 
 const App = () => {
-    const [query, setQuery] = useState('');
-    const [weather, setWeather] = useState({});
-    
-    const search = async (e) => {
-        if(e.key === 'Enter') {
-            const data = await fetchWeather(query);
+  const [query, setQuery] = useState('');
+  const [weather, setWeather] = useState({});
 
-            setWeather(data);
-            setQuery('');
-        }
+  const search = async (e) => {
+    if (e.key === 'Enter') {
+      const data = await fetchWeather(query);
+
+      setWeather(data);
+      setQuery('');
     }
+  };
 
-    useEffect(async()=>{
-        if(query === ''){
-            const data = await fetchWeather('iran');
-            setWeather(data);
-        }
-    },[])
+  const initializeProject = useCallback(async () => {
+    if (query === '' && weather.main === undefined) {
+      const data = await fetchWeather('iran');
+      setWeather(data);
+    }
+  }, [query, weather]);
 
-    return (
-        <div className="main-container">
-            <input type="text"className="search"placeholder="Search..."value={query}onChange={(e) => setQuery(e.target.value)}onKeyPress={search}/>
-            {weather.main && (
-                <div className="city">
-                    <h2 className="city-name">
-                        <span>{weather.name}</span>
-                        <sup>{weather.sys.country}</sup>
-                    </h2>
-                    <div className="city-temp">
-                        {Math.round(weather.main.temp)}
-                        <sup>&deg;C</sup>
-                    </div>
-                    <div className="info">
-                        <img className="city-icon" src={`https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`} alt={weather.weather[0].description} />
-                        <p>{weather.weather[0].description}</p>
-                    </div>
-                </div>
-            )}
-        </div>
-    );
-}
+  useEffect(() => {
+    initializeProject();
+  }, [initializeProject]);
+
+  return (
+    <div className="main-container">
+      <input
+        type="text"
+        className="search"
+        placeholder="Search..."
+        value={query}
+        onChange={(e) => setQuery(e.target.value)}
+        onKeyPress={search}
+      />
+
+      <div className="city">
+        {weather.main ? <WeatherView weather={weather} /> : <Loading />}
+      </div>
+    </div>
+  );
+};
 
 export default App;
